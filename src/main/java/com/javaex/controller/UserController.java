ca96ee5c702b630566ffde592d250e0c95edd962
Ryeokyoung <ryeokyoungk@gmail.com>
2022-06-21 15:01:24 +0900

    -

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.javaex.service.UserService;
 import com.javaex.vo.UserVo;
 
+@Controller
 @WebServlet("/user")
 public class UserController {
 	// 필드
